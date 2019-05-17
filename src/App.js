1749e3a73ff82575431b1657930e17ba141879e0
mnorty <mnorty@gmail.com>
2019-05-17 13:26:28 -0600

    updated prices

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ import './App.css';
 let products = [
   {name: 'Hammer',
    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9r45BA7KAUn9JAMi8wp5FDx1CJqXP20ydUc_W13k7k29oZ2jOHA',
-   price: 0},
+   price: 5},
   {name: 'Shovel',
    img: 'https://www.carspiritpk.com/wp-content/uploads/2018/12/Annie-VW-Beetle-11-850x567-726x363.jpg',
-   price: 1},
+   price: 10},
   {name: 'Rake',
    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBI3t3-RhTBocQAh-_MpmPTVZNyZZIjpUi4XiJo-qcq4k-Ym1x',
-   price: 2},
+   price: 15},
    {name: 'Gloves',
    img: 'https://barrettjacksoncdn.azureedge.net/staging/carlist/items/Fullsize/Cars/218069/218069_Side_Profile_Web.jpg',
-   price: 3}
+   price: 20}
 ]
 
 class App extends Component {

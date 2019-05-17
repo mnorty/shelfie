c51c10539ea841012d1ded6914d4a0a4ec87b3f7
mnorty <mnorty@gmail.com>
2019-05-17 11:32:05 -0600

    handleimg working

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,12 +2,31 @@ import React,{Component} from 'react'
 import axios from 'axios'
 
 class Form extends Component {
-  // constructor(){
+  constructor(props){
+    super(props);
+    this.state = {
+      img:'',
+      name:'',
+      price: 0
+    }
+  }
 
-  // }
+  handleImg(img) {
+    this.setState({img:img})
+    console.log(this.state.img)
+  }
+
   render(){
     return(
-    <div>Form</div>
+    <div>
+      <div>Form
+        <input type="text" onChange={e => this.handleImg(e.target.value)}/>
+        <input type="text"/>
+        <input type="text"/>
+        <button>Cancel</button>
+        <button>Add to Inventory</button>
+      </div>
+    </div>
     )
   }
 }

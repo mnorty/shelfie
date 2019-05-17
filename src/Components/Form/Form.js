3696a348eaf86f90c8e0a9df6800544f9bfd7514
mnorty <mnorty@gmail.com>
2019-05-17 11:45:45 -0600

    can enter into the fields and clear them now

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -16,15 +16,35 @@ class Form extends Component {
     console.log(this.state.img)
   }
 
+  handleName(name) {
+    this.setState({name:name})
+    console.log(this.state.name)
+  }
+
+  handlePrice(price) {
+    this.setState({price:price})
+    console.log(this.state.price)
+  }
+
+  clearInputs(){
+    this.setState({
+      img:'',
+      name:'',
+      price: 0
+    })
+  }
+
   render(){
     return(
     <div>
       <div>Form
-        <input type="text" onChange={e => this.handleImg(e.target.value)}/>
-        <input type="text"/>
-        <input type="text"/>
-        <button>Cancel</button>
-        <button>Add to Inventory</button>
+        <input type="text" value={this.state.img} onChange={e => this.handleImg(e.target.value)}/>
+        <input type="text" value={this.state.name} onChange={e => this.handleName(e.target.value)}/>
+        <input type="text" value={this.state.price} onChange={e => this.handlePrice(e.target.value)}/>
+        <div className="ButtonBox">
+          <button onClick={_ => this.clearInputs()}>Cancel</button>
+          <button>Add to Inventory</button>
+        </div>
       </div>
     </div>
     )
